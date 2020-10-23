99f09630ae3e9a275030e34cc2555cf33204f5ce
ifer-itcast <yangkangkang@itcast.cn>
2020-10-23 23:50:54 +0800

    为注册表单设置校验规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,6 +16,16 @@ $(function () {
     // 自定义校验规则
     var form = layui.form;
     form.verify({
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格']
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败，则 return 一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致！';
+            }
+        }
     });
 });
