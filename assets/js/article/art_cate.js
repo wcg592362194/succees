4cf5b21291e143034bd75b69333ce80c446a0c28
ifer-itcast <yangkangkang@itcast.cn>
2020-10-25 00:44:01 +0800

    使用layer.open实现弹出层

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,4 +1,5 @@
 $(function () {
+    var layer = layui.layer;
     // 获取文章分类的列表
     initArtCateList();
 
@@ -12,4 +13,14 @@ $(function () {
             }
         });
     }
+
+    // 添加类别弹框
+    $('#btnAddCate').on('click', function() {
+        layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: ''
+        });
+    });
 });
