53e2e6ca0e501c703d2d09c1676c11bcfd2a1557
ifer-itcast <yangkangkang@itcast.cn>
2020-10-24 23:36:43 +0800

    实现裁剪区域图片的替换

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,4 +1,5 @@
 $(function () {
+    var layer = layui.layer;
     // 1.1 获取裁剪区域的 DOM 元素
     const $image = $('#image')
     // 1.2 配置选项
@@ -14,5 +15,15 @@ $(function () {
     // 选择文件的功能
     $('#btnChooseImage').on('click', function () {
         $('#file').click();
+    });
+
+    // 为文件选择框绑定 change 事件
+    $('#file').on('change', function (e) {
+        var filelist = e.target.files;
+        if (filelist.length === 0) {
+            return layer.msg('请选择照片！')
+        }
+        var newImgURL = URL.createObjectURL(filelist[0]);
+        $image.cropper('destroy').attr('src', newImgURL).cropper(options);
     });
 });
