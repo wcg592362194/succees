8b275119aecf44072260a454e18a57fbde29ff18
ifer-itcast <yangkangkang@itcast.cn>
2020-10-24 22:09:10 +0800

    使用form.val方法快速为基本资料表单赋值

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -19,7 +19,9 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg('获取用户信息失败！');
                 }
-                console.log(res);
+                console.log(res.data);
+                // 调用 form.val() 快速为表单赋值
+                form.val('formUserInfo', res.data);
             }
         });
     }
