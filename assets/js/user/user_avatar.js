84d4961abc5de5aff8221bd20dedd73b389816e6
ifer-itcast <yangkangkang@itcast.cn>
2020-10-24 23:58:55 +0800

    将裁剪后的头像上传到服务器

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -26,4 +26,30 @@ $(function () {
         var newImgURL = URL.createObjectURL(filelist[0]);
         $image.cropper('destroy').attr('src', newImgURL).cropper(options);
     });
+
+    // 将裁剪后的图片上传到服务器
+    $('#btnUpload').on('click', function () {
+        // 1. 拿到用户裁剪之后的头像
+        const dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        // 2. 调用接口，把头像上传到服务器
+        $.ajax({
+            method: 'POST',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更换头像失败！');
+                }
+                layer.msg('更换头像成功！');
+                window.parent.getUserInfo();
+            }
+        });
+    });
 });
