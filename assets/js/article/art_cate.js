f3f6468a89633ea0dd45f5a81eb01b2fbd04b8fb
ifer-itcast <yangkangkang@itcast.cn>
2020-10-25 00:52:52 +0800

    在弹出框中渲染添加类别的表单结构

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -15,12 +15,12 @@ $(function () {
     }
 
     // 添加类别弹框
-    $('#btnAddCate').on('click', function() {
+    $('#btnAddCate').on('click', function () {
         layer.open({
             type: 1,
             area: ['500px', '250px'],
             title: '添加文章分类',
-            content: ''
+            content: $('#dialog-add').html()
         });
     });
 });
