8fd4eb07a727b7c895fd07fb73b01305fc7a66df
ifer-itcast <yangkangkang@itcast.cn>
2020-10-24 21:11:32 +0800

    渲染用户头像

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,8 +11,30 @@ function getUserInfo() {
         headers: {
             Authorization: localStorage.getItem('token') || ''
         },
-        success: function(res) {
-            console.log(res);
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！');
+            }
+            renderAvatar(res.data);
         }
     });
+}
+
+function renderAvatar(user) {
+    // 优先 nickname
+    // 1. 获取用户名称
+    var name = user.nickname || user.username;
+    // 2. 设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;' + name);
+    // 3. 按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 3.2 渲染文本
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
 }
