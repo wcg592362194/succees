9feb0437fab9064900ed7f57c560000103713785
ifer-itcast <yangkangkang@itcast.cn>
2020-10-25 01:01:15 +0800

    添加文章分类功能

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -15,12 +15,34 @@ $(function () {
     }
 
     // 添加类别弹框
+    var indexAdd = null;
     $('#btnAddCate').on('click', function () {
-        layer.open({
+        indexAdd = layer.open({
             type: 1,
             area: ['500px', '250px'],
             title: '添加文章分类',
             content: $('#dialog-add').html()
+        });
+    });
+
+    // 添加文章分类，通过委托的形式为表单绑定提交事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！');
+                }
+                // 重新获取分类数据
+                initArtCateList();
+                // 提示成功消息
+                layer.msg('新增分类成功！');
+                // 关闭弹出层
+                layer.close(indexAdd);
+            }
         });
     });
 });
