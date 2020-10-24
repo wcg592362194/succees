fe4e9da11d605af4fc77440d9d88e6260861612d
ifer-itcast <yangkangkang@itcast.cn>
2020-10-25 00:38:32 +0800

    获取文章分类数据并使用模板引擎渲染到页面

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,15 @@
+$(function () {
+    // 获取文章分类的列表
+    initArtCateList();
+
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                var htmlStr = template('tpl-table', res);
+                $('tbody').html(htmlStr);
+            }
+        });
+    }
+});
