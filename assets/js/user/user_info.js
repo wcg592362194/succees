4372b3b5130ef1741bae458f0c06b349d6222349
ifer-itcast <yangkangkang@itcast.cn>
2020-10-24 22:04:28 +0800

    获取用户的基本信息

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,5 +1,6 @@
 $(function () {
     var form = layui.form;
+    var layer = layui.layer;
     form.verify({
         nickname: function (value) {
             if (value.length > 6) {
@@ -7,4 +8,20 @@ $(function () {
             }
         }
     });
+
+    initUserInfo();
+    // 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！');
+                }
+                console.log(res);
+            }
+        });
+    }
+
 });
