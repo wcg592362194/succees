4f4ea1bf7ccc0b8d564b2abf3d3c0361ae920d11
ifer-itcast <yangkangkang@itcast.cn>
2020-10-26 01:19:15 +0800

    准备cover_img字段

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -64,8 +64,18 @@ $(function () {
         // 基于 form 表单快速创建一个 FormData 对象
         var fd = new FormData($(this)[0]);
         fd.append('state', art_state);
-        fd.forEach(function (v, k) {
+        /* fd.forEach(function (v, k) {
             console.log(k, v);
-        });
+        }); */
+
+        $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) { // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                fd.append('cover_img', blob);
+            });
     });
 });
