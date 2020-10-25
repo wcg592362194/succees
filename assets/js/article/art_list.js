d140c683bca54713895f0dca2b748f7a619d35d0
ifer-itcast <yangkangkang@itcast.cn>
2020-10-25 23:09:01 +0800

    定义模板中格式化时间的过滤器

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,5 +1,23 @@
 $(function () {
     var layer = layui.layer;
+
+    function padZero(n) {
+        return n > 9 ? n : '0' + n;
+    }
+    // 格式化时间的过滤器
+    template.defaults.imports.dateFormat = function (date) {
+        const dt = new Date(date);
+        var y = dt.getFullYear();
+        var m = padZero(dt.getMonth() + 1);
+        var d = padZero(dt.getDate());
+
+        var hh = padZero(dt.getHours());
+        var mm = padZero(dt.getSeconds());
+        var ss = padZero(dt.getSeconds());
+
+        return `${y}-${m}-${d} ${hh}:${mm}:${ss}`;
+    };
+
     // 查询参数对象
     var q = {
         pagenum: 1, // 页码
