731f315ec534b344e6dfa4a93b2c35d324804f8e
ifer-itcast <yangkangkang@itcast.cn>
2020-10-25 22:30:26 +0800

    删除文章分类

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -88,4 +88,26 @@ $(function () {
             }
         });
     });
+
+    // 通过委托的形式为删除按钮绑定点击事件
+    $('tbody').on('click', '.btn-delete', function () {
+        var id = $(this).attr('data-id');
+        layer.confirm('确认删除?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: `/my/article/deletecate/${id}`,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除分类失败！');
+                    }
+                    layer.msg('删除分类成功！');
+                    layer.close(index);
+                    initArtCateList();
+                }
+            });
+        });
+    });
 });
