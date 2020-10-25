314f71b7c680d0faa59a9e36efefe1d9b603ddc1
ifer-itcast <yangkangkang@itcast.cn>
2020-10-26 00:48:34 +0800

    渲染文章标题和文章类别界面

---
+++ assets/js/article/art_pub.js
@@ -0,0 +1,22 @@
+$(function () {
+    var layer = layui.layer;
+    var form = layui.form;
+    // 加载文章分类
+    initCate();
+
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('初始化文章分类失败！');
+                }
+                // 使用模板引擎，渲染分类的下拉菜单
+                var htmlStr = template('tpl-cate', res);
+                $('[name=cate_id]').html(htmlStr);
+                form.render();
+            }
+        });
+    }
+});
