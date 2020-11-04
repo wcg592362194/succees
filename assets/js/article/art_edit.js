e4ace1c2f7024790315a91237ec518e9f4981154
ifer-itcast <yangkangkang@itcast.cn>
2020-11-04 16:02:39 +0800

    修改文章

--- assets/js/article/art_edit.js
+++ assets/js/article/art_edit.js
@@ -24,11 +24,11 @@ $(function () {
 
     // // 1. 初始化图片裁剪器
     var $image = $('#image');
-    // // 2. 裁剪选项
-    // var options = {
-    //     aspectRatio: 400 / 280,
-    //     preview: '.img-preview'
-    // };
+    // 2. 裁剪选项
+    var options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    };
     // // 3. 初始化裁剪区域
     // $image.cropper(options);
 
